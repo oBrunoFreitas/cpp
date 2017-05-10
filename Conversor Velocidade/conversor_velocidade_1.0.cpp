d2147ba58b12043bbf744dcaf92123262c6bb037
Bruno Freitas <bruno_freitas@live.com>
2017-05-10 17:27:02 -0300

    Update conversor_velocidade_1.0.cpp

--- Conversor Velocidade/conversor_velocidade_1.0.cpp
+++ Conversor Velocidade/conversor_velocidade_1.0.cpp
@@ -8,7 +8,7 @@ int ms, kmh;
 
 
 	cout << "Digite a velocidade em M/S para ser transformada em KM/H \n";
-	cin>> ms;
+	cin >> ms;
 	kmh=ms*3.6;
 	cout << "A velocidade em KMH e " << kmh << "\n" ;
 	system("pause");
